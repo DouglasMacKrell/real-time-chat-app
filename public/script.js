4a44dcc295d1f5840b58d12d0de81d3a816c7826
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-12-14 22:19:50 -0500

    Room addition init

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const socket = io("http://localhost:3000");
 
 const messageContainer = document.getElementById("message-container");
+const roomContainer = document.getElementById("room-container");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 
@@ -17,6 +18,16 @@ if (messageForm != null) {
     messageInput.value = "";
   });
 }
+
+socket.on('room-created', room => {
+  const roomElement = document.createElement('div')
+  roomElement.innerText = room
+  const roomLink = document.createElement("a")
+  roomLink.href =  `/${room}`
+  roomLink.innerText = 'Join: '
+  roomContainer.append(roomElement)
+  roomContainer.append(roomLink)
+})
 
 socket.on("chat-message", (data) => {
   appendMessage(`${data.name}: ${data.message}`);
--- server.js
+++ server.js
@@ -25,9 +25,13 @@ app.post('/room', (req, res) => {
   rooms[req.body.room] = { users: {} }
   res.redirect(req.body.room)
   // send message that new room was created
+  io.emit('room-created', req.body.room)
 })
 
 app.get('/:room', (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect("/")
+  }
   res.render('room', { roomName: req.params.room })
 })
 
